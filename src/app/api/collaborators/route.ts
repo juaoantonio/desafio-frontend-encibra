bd8ed53419a8db26f9a4e1792c493d838e70e365
João Antônio Barbosa <joaobarbosadev@gmail.com>
2023-10-22 18:43:55 -0300

    feat: added api endpoint for Collaborators

---
+++ src/app/api/collaborators/route.ts
@@ -0,0 +1,71 @@
+import { getUsersParamsSchema } from '@/schemas'
+import { CollaboratorService } from '@/services'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+  const result = getUsersParamsSchema.safeParse(searchParams)
+  const userId = req.cookies.get('id')?.value
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        message: 'Você precisa estar logado para acessar essa rota',
+      },
+      { status: 401 },
+    )
+  }
+
+  if (!result.success) {
+    return NextResponse.json(
+      {
+        message: result.error.message,
+      },
+      { status: 400 },
+    )
+  }
+
+  const { current } = result.data
+
+  if (!current) {
+    const collaborators =
+      await CollaboratorService.getCollaboratorWithoutCurrent(+userId, true)
+
+    return NextResponse.json(
+      {
+        collaborators,
+      },
+      { status: 200 },
+    )
+  }
+
+  const collaborators = await CollaboratorService.getCollaborators()
+
+  return NextResponse.json(
+    {
+      collaborators,
+    },
+    { status: 200 },
+  )
+}
+
+export async function POST(req: NextRequest) {
+  const collaborator = await req.json()
+  const result = await CollaboratorService.createCollaborator(collaborator)
+
+  if (result.error) {
+    return NextResponse.json({ message: result.message }, { status: 400 })
+  }
+
+  if (!result.collaborator) {
+    throw new Error('Collaborator not found')
+  }
+
+  return NextResponse.json(
+    {
+      email: result.collaborator.email,
+      id: result.collaborator.id,
+    },
+    { status: 201 },
+  )
+}
