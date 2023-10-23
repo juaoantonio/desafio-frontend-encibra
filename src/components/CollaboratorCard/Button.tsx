1d482ebe60e829d07cd00adfa062cafbd3bb4d7a
João Antônio Barbosa <joaobarbosadev@gmail.com>
2023-10-23 14:29:56 -0300

    fix: replaced native `<a>` component for `<Link>` next component

--- src/components/CollaboratorCard/Button.tsx
+++ src/components/CollaboratorCard/Button.tsx
@@ -1,14 +1,18 @@
+import Link from 'next/link'
 import { ComponentProps } from 'react'
 
-type ButtonProps = ComponentProps<'a'>
+type ButtonProps = {
+  children: React.ReactNode
+  href: string
+}
 
 export function Button({ children, ...props }: ButtonProps) {
   return (
-    <a
+    <Link
       className="bg-gray-900 text-white font-medium py-2 px-8 rounded-lg text-xl mt-6 inline-block "
       {...props}
     >
       {children}
-    </a>
+    </Link>
   )
 }
--- src/components/ProjectCard/Button.tsx
+++ src/components/ProjectCard/Button.tsx
@@ -1,14 +1,18 @@
-import { ComponentProps } from 'react'
+import Link from 'next/link'
 
-type ButtonProps = ComponentProps<'a'>
+type ButtonProps = {
+  children: React.ReactNode
+  href: string
+}
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, href, ...props }: ButtonProps) {
   return (
-    <a
+    <Link
+      href={href}
       className="bg-gray-900 text-white font-medium py-2 px-8 rounded-lg text-xl mt-6 inline-block "
       {...props}
     >
       {children}
-    </a>
+    </Link>
   )
 }
