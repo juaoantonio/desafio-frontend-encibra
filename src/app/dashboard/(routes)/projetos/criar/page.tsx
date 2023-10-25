f2a52736f5e47a4634ad633128fb0a2810d3814d
João Antônio Barbosa <joaobarbosadev@gmail.com>
2023-10-25 00:05:02 -0300

    fix: added private route to route that handle project creation

--- src/app/dashboard/(routes)/projetos/criar/page.tsx
+++ src/app/dashboard/(routes)/projetos/criar/page.tsx
@@ -1,12 +1,15 @@
 import { fetchCollaborators } from '@/lib/utils'
 import { CreateProjectForm } from './components/CreateProjectForm'
+import { PrivateRoute } from '@/components/PrivateRoute'
 
 export default async function CreateProjectPage() {
   const collaborators = await fetchCollaborators(true)
 
   return (
-    <main className="mx-auto max-w-2xl py-10 px-5">
-      <CreateProjectForm collaborators={collaborators} />
-    </main>
+    <PrivateRoute>
+      <main className="mx-auto max-w-2xl py-10 px-5">
+        <CreateProjectForm collaborators={collaborators} />
+      </main>
+    </PrivateRoute>
   )
 }
